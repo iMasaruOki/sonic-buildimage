e185e61cb8ae354681d8f3254f94f84d1303c59f
Masaru OKI <m-oki@iij.ad.jp>
2021-06-30 10:52:27 +0000

    Execute config interface ip command.

--- files/image_config/keepalived/notify.sh
+++ files/image_config/keepalived/notify.sh
@@ -3,5 +3,18 @@
 # $2: Interface
 # $3: new state (MASTER/BACKUP/FAULT/STOP)
 
-touch /var/log/keepalive.log
-echo "Become $3 on $2" >> /var/log/keepalived.log
+LOGFILE=/var/log/keepalived.log
+sudo touch $LOGFILE
+echo "Become $3 on $2" |sudo tee -a $LOGFILE
+
+case $3 in
+    MASTER)
+	sudo config interface ip add $2 $1
+	;;
+    BACKUP)
+	sudo config interface ip remove $2 $1
+	;;
+    *)
+	;;
+esac
+exit 0
