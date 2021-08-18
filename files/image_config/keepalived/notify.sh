57fc47262c68dff09d7e22500465b1c953dbc6d0
Masaru OKI <m-oki@iij.ad.jp>
2021-08-18 23:59:55 +0000

    Control downlink.

--- files/image_config/keepalived/notify.sh
+++ files/image_config/keepalived/notify.sh
@@ -2,17 +2,23 @@
 # $1: Virtual IP address
 # $2: Interface
 # $3: new state (MASTER/BACKUP/FAULT/STOP)
+# $4: downlink inteface #0
+# $5: downlink inteface #1
 
 LOGFILE=/var/log/keepalived.log
 sudo touch $LOGFILE
-echo "Become $3 on $2" |sudo tee -a $LOGFILE
+echo "Become $3 on $2 (VIP $1, downlink $4, $5)" |sudo tee -a $LOGFILE
 
 case $3 in
     MASTER)
 	sudo config interface ip add $2 $1
+	sudo config interface startup $4
+	sudo config interface startup $5
 	;;
     BACKUP)
 	sudo config interface ip remove $2 $1
+	sudo config interface shutdown $4
+	sudo config interface shutdown $5
 	;;
     *)
 	;;
