6afe7325e8cfceb65eb39ec52fb992aa7d516555
Masaru OKI <m-oki@iij.ad.jp>
2021-08-19 19:32:24 +0000

    Add support downlink interface is empty case.

--- files/image_config/keepalived/notify.sh
+++ files/image_config/keepalived/notify.sh
@@ -12,13 +12,13 @@ echo "Become $3 on $2 (VIP $1, downlink $4, $5)" |sudo tee -a $LOGFILE
 case $3 in
     MASTER)
 	sudo config interface ip add $2 $1
-	sudo config interface startup $4
-	sudo config interface startup $5
+        test "x$4" != "x" && sudo config interface startup $4
+        test "x$5" != "x" && sudo config interface startup $5
 	;;
     BACKUP)
 	sudo config interface ip remove $2 $1
-	sudo config interface shutdown $4
-	sudo config interface shutdown $5
+        test "x$4" != "x" && sudo config interface shutdown $4
+        test "x$5" != "x" && sudo config interface shutdown $5
 	;;
     *)
 	;;
